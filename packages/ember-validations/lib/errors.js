e563cd39f8eacfcc7aa1e5571cc654b7e032afe1
Brian Cardarella <bcardarella@gmail.com>
2013-02-26 23:55:08 -0500

    Set to undefined when clearing

--- packages/ember-validations/lib/errors.js
+++ packages/ember-validations/lib/errors.js
@@ -5,6 +5,7 @@ Ember.Validations.Errors = Ember.Object.extend({
   clear: function() {
     var keys = Object.keys(this);
     for(var i = 0; i < keys.length; i++) {
+      this.set(keys[i], undefined);
       delete this[keys[i]];
     }
   }
