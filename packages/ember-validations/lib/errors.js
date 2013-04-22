81c319c3ac57ee3983bd3ba0c823702c724d4c77
Vincenzo <mastropinguino@networky.net>
2013-04-22 21:49:45 +0200

    Support for clearing only subset errors

--- packages/ember-validations/lib/errors.js
+++ packages/ember-validations/lib/errors.js
@@ -4,7 +4,22 @@ Ember.Validations.Errors = Ember.Object.extend({
   },
   clear: function() {
     var keys = Object.keys(this);
+    var only = null;
+    if ( arguments.length > 1 )
+    {
+      only = Array.prototype.slice.apply(arguments);
+    }
+    else if (arguments.length === 1)
+    {
+      if ( arguments[0] instanceof Array )
+        only = arguments[0];
+      else
+        only = [arguments[0]];
+    }
     for(var i = 0; i < keys.length; i++) {
+      if ( only && only.indexOf(keys[i]) < 0 )
+        continue;
+
       this.set(keys[i], undefined);
       delete this[keys[i]];
     }
