b07ca8a7b3ffb29b19b2558181736655a3fe2ed1
Marin Abernethy <maabernethy@gmail.com>
2015-04-06 15:04:19 -0400

    Fixes validate properties test helpers
    test helpers were validating wrong value in array due to run loop and
    promise issues. So, the order of values in the array passed to the test helper functions would
    affect the results.
    
    This PR chains the promises to run sequentially to fix this problem.

--- tests/unit/helpers/validate-properties-test.js
+++ tests/unit/helpers/validate-properties-test.js
@@ -4,6 +4,8 @@ import {
   testInvalidPropertyValues
 } from '../../helpers/validate-properties';
 
+var _deepEqual;
+
 moduleFor('controller:foo', 'Unit - Foo Controller Test', {
   needs: [
     'ember-validations@validator:local/presence',
@@ -24,5 +26,54 @@ testInvalidPropertyValues('baz', ['', null, undefined], function(subject) {
 });
 
 testValidPropertyValues('baz', ['Winston', '12345', null, undefined, ''], function(subject) {
+  subject.set('isBaz', false);
+});
+
+
+moduleFor('controller:foo', 'Unit - Ensure validate properties test helpers fail when invalid', {
+  needs: [
+    'ember-validations@validator:local/presence',
+    'ember-validations@validator:local/length'
+  ],
+
+  beforeEach: function(assert) {
+    // use inverse of deepEqual to ensure the test helpers fail when invalid
+    assert.deepEqual = assert.notDeepEqual;
+  }
+});
+
+testValidPropertyValues('bar', [undefined, 'Winston', '12345']);
+testValidPropertyValues('bar', ['Winston', undefined, '12345']);
+testValidPropertyValues('bar', ['Winston', '12345', undefined]);
+
+testInvalidPropertyValues('bar', ['', null, undefined, 'abc', 'Winston']);
+testInvalidPropertyValues('bar', ['Winston', null, undefined, 'abc']);
+testInvalidPropertyValues('bar', [null, 'Winston', undefined, 'abc']);
+
+testInvalidPropertyValues('baz', ['Winston', '12345'], function(subject) {
+  subject.set('isBaz', true);
+});
+
+testValidPropertyValues('baz', [undefined, 'Winston', '12345'], function(subject) {
+  subject.set('isBaz', true);
+});
+testValidPropertyValues('baz', ['Winston', '12345', undefined], function(subject) {
+  subject.set('isBaz', true);
+});
+testValidPropertyValues('baz', ['Winston', undefined, '12345'], function(subject) {
+  subject.set('isBaz', true);
+});
+
+testInvalidPropertyValues('baz', ['', null, undefined, 'Winston'], function(subject) {
+  subject.set('isBaz', true);
+});
+testInvalidPropertyValues('baz', ['Winston', null, undefined], function(subject) {
+  subject.set('isBaz', true);
+});
+testInvalidPropertyValues('baz', ['', null, 'Winston', undefined], function(subject) {
+  subject.set('isBaz', true);
+});
+
+testInvalidPropertyValues('baz', ['Winston', '12345', null, undefined, ''], function(subject) {
   subject.set('isBaz', false);
 });
