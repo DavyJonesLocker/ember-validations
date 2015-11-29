59d8f3df3921df5cc3c2c60c26091394e5015d3c
Josh Cope <jcope2010@gmail.com>
2015-11-29 00:47:17 -0500

    update ember try to 0.0.8, add configs for latest 1.13, release, beta, canary, official .12.1

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,9 +7,49 @@ module.exports = {
       }
     },
     {
-      name: "ember-1.12.0-beta.1",
+      name: "ember-1.12.1",
       dependencies: {
-        "ember": "1.12.0-beta.1"
+        "ember": "1.12.1"
+      }
+    },
+    {
+      name: "ember-1.13",
+      dependencies: {
+        "ember": "1.13.11",
+        "ember-data": "1.13.15"
+      }
+    },
+    {
+      name: 'ember-release',
+      dependencies: {
+        'ember': 'components/ember#release',
+        'ember-data': 'components/ember-data#release'
+      },
+      resolutions: {
+        'ember': 'release',
+        'ember-data': 'release'
+      }
+    },
+    {
+      name: 'ember-beta',
+      dependencies: {
+        'ember': 'components/ember#beta',
+        'ember-data': 'components/ember-data#beta'
+      },
+      resolutions: {
+        'ember': 'beta',
+        'ember-data': 'beta'
+      }
+    },
+    {
+      name: 'ember-canary',
+      dependencies: {
+        'ember': 'components/ember#canary',
+        'ember-data': 'components/ember-data#canary'
+      },
+      resolutions: {
+        'ember': 'canary',
+        'ember-data': 'canary'
       }
     }
   ]
